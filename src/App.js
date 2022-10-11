dc26f45af65539a55cc91c7e87daf8330c58b5d3
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-10-11 10:13:45 +0600

    navbar is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-
+import Blogs from './components/Blogs/Blogs';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+import Main from './Layout/Main';
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        { path: '/', element: <Topics></Topics> },
+        { path: 'statistics', element: <Statistics></Statistics> },
+        { path: 'blogs', element: <Blogs></Blogs> },
+        // {
+        //   path: 'friends',
+        //   loader: async () =>{
+        //     return fetch('https://jsonplaceholder.typicode.com/users');
+        //   },
+        //   element: <Friends></Friends>
+        // },
+        // {
+        //   path:'/friend/:friendId',
+        //   loader: async ({params}) =>{
+        //     return fetch(`https://jsonplaceholder.typicode.com/users/${params.friendId}`)
+        //   },
+        //   element: <FriendDetails></FriendDetails>
+        // },
+        // {
+        //   path: 'posts',
+        //   loader: async() =>{
+        //     return fetch('https://jsonplaceholder.typicode.com/posts')
+        //   },
+        //   element: <Posts></Posts>
+        // },
+        // {
+        //   path: '/post/:postId',
+        //   loader: async({params}) =>{
+        //     return fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+        //   },
+        //   element: <PostDetails></PostDetails>
+        // }
+      ]
+    },
+    { path: '*', element: <div>This route not found: 404</div> }
+  ])
   return (
     <div className="App">
-      <h1>Assignment is going to be start now</h1>
-      <h2>Github is working nicely</h2>
+        <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,32 +1,19 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import './Navbar.css'
 const Navbar = () => {
     return (
-        <div>
-            <div className="navbar-container">
-                <nav className="navbar navbar-expand-lg bg-light">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="./">Test Your Skills, Fest Your Career</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarText">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                <a className="nav-link active" href="./">Topics</a>
-                                </li>
-                                <li className="nav-item">
-                                <a className="nav-link" href="./statistics">Statistics</a>
-                                </li>
-                                <li className="nav-item">
-                                <a className="nav-link" href="./blog">Blog</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+        <div className='header'>
+            <div className="title">
+                <img className='logo-img' src="https://png.pngtree.com/png-vector/20210502/ourlarge/pngtree-quiz-png-transparent-background-png-image_3257718.jpg" alt="" />
+                <h2 className='logo-title'>Test Your Knowledge</h2>
             </div>
-        </div>
+            <nav className='nav-items'>
+                <NavLink to='/'>Topics</NavLink>
+                <NavLink to='/statistics'>Statistics</NavLink>
+                <NavLink to="/blogs">Blogs</NavLink>
+            </nav>
+    </div>
     );
 };
 
