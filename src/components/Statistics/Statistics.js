f817a0116203c31a8eafd08003931caea8321e89
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-10-12 12:15:44 +0600

    make website responsive

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import './Statistics.css'
 
 const Statistics = () => {
     const topicsData = useLoaderData();
@@ -8,23 +9,29 @@ const Statistics = () => {
 
       
     return (
-        <AreaChart
-            width={1200}
-            height={400}
-            data={data}
-            margin={{
-                top: 100,
-                right: 150,
-                left: 150,
-                bottom: 0
-            }}
-            >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
-        </AreaChart>
+        <div className="chart">
+            <div className="chart_container">
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart
+                    width={1200}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 50,
+                        right: 50,
+                        left: 50,
+                        bottom: 0
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
+                </AreaChart>
+            </ResponsiveContainer>
+            </div>
+        </div>
     );
 };
 
